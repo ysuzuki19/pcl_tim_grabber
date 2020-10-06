32d75d75d8e83bbc7853e0131e81ddbcce07148e
ysuzuki19 <42496938+ysuzuki19@users.noreply.github.com>
2020-10-06 21:03:17 +0900

    Edit by code review

--- include/pcl/io/tim_grabber.h
+++ include/pcl/io/tim_grabber.h
@@ -1,41 +1,12 @@
 /*
- * Software License Agreement (BSD License)
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
- *  Copyright (c) 2014-, Open Perception, Inc.
+ *  Copyright (c) 2020-, Open Perception
  *  Copyright (c) 2020, ysuzuki19
  *
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the copyright holder(s) nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
+ *  All rights reserved
  */
-
 
 #pragma once
 
@@ -46,6 +17,7 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 #include <vector>
 #include <string>
 #include <thread>
@@ -53,94 +25,97 @@
 namespace pcl
 {
 
-  class PCL_EXPORTS TimGrabber : public Grabber
-  {
-    public:
-      using sig_cb_sick_tim_scan_point_cloud_xyz = void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&);
+class PCL_EXPORTS TimGrabber : public Grabber
+{
+  public:
+    using sig_cb_sick_tim_scan_point_cloud_xyz = void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&);
 
-      TimGrabber ();
-      TimGrabber (const boost::asio::ip::address& ipAddress, const std::uint16_t port);
-      ~TimGrabber () noexcept;
+    TimGrabber ();
+    TimGrabber (const boost::asio::ip::address& ipAddress, const std::uint16_t port);
+    ~TimGrabber () noexcept;
 
-      void
-      start () override;
+    void
+    start () override;
 
-      void
-      stop () override;
+    void
+    stop () override;
 
-      std::string
-      getName () const override;
+    std::string
+    getName () const override;
 
-      bool
-      isRunning () const override;
+    bool
+    isRunning () const override;
 
-    protected:
-      pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_xyz_ptr_;
-      boost::signals2::signal<sig_cb_sick_tim_scan_point_cloud_xyz>* point_cloud_xyz_signal_;
+  protected:
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_xyz_ptr_;
+    boost::signals2::signal<sig_cb_sick_tim_scan_point_cloud_xyz>* point_cloud_xyz_signal_;
 
-      void
-      publishSignals ();
+    void
+    publishSignal ();
 
-    private:
-      constexpr static float angle_start_ = - 1.0 * M_PI / 4.0;
-      constexpr static float angle_range_ = 2.0 * M_PI * 3.0 / 4.0;
+  private:
+    constexpr static float angle_start_ = - 1.0 * M_PI / 4.0;
+    constexpr static float angle_range_ = 2.0 * M_PI * 3.0 / 4.0;
 
-      //// cos and sin lookup table for calculaing coordinate
-      //// reset lookup table if amount of received data is different
-      std::vector<float> cos_dynamic_lookup_table_;
-      std::vector<float> sin_dynamic_lookup_table_;
+    //// lookup tables for calculaing 2d-coordinate
+    //// reset lookup tables if amount of received data is different
+    std::vector<float> cos_dynamic_lookup_table_;
+    std::vector<float> sin_dynamic_lookup_table_;
 
-      constexpr static boost::array<char,21> scan_query_ = {"\x02sRN LMDscandata 1\x03\0"};
-      boost::array<char, 4000> received_packet_;
-      std::size_t length_;
+    std::array<char, 4000> received_packet_;
+    std::size_t length_;
 
-      std::size_t amount_of_data_;
-      std::vector<float> distances_;
+    std::size_t amount_of_data_;
+    std::vector<float> distances_;
 
-      boost::asio::ip::tcp::endpoint tcp_endpoint_;
-      boost::asio::io_context tim_io_context_;
-      boost::asio::ip::tcp::socket tim_socket_;
-      //// wait time for receiving data
-      unsigned int wait_time_milliseconds_ = 0;
+    boost::asio::ip::tcp::endpoint tcp_endpoint_;
+    boost::asio::io_service tim_io_service_;
+    boost::asio::ip::tcp::socket tim_socket_;
+    //// wait time for receiving data (on the order of milliseconds)
+    unsigned int wait_time_milliseconds_ = 0;
 
-      pcl::EventFrequency frequency_;
-      mutable std::mutex frequency_mutex_;
+    pcl::EventFrequency frequency_;
+    mutable boost::mutex frequency_mutex_;
 
-      std::thread grabber_thread_;
-      bool is_running_ = false;
+    std::thread grabber_thread_;
+    bool is_running_ = false;
 
-      void
-      initialize ();
+    void
+    initialize ();
 
-      float
-      getFramesPerSecond () const;
+    float
+    getFramesPerSecond () const override;
 
-      void
-      buildLookupTable ();
+    void
+    buildLookupTables ();
 
-      //// check size of lookup table
-      //// rebuild if lookup table have different size
-      void
-      updateLookupTable ();
+    //// check size of lookup tables
+    //// rebuild if lookup tables have different size
+    void
+    updateLookupTables ();
 
-      //// check received packet is valid
-      bool
-      isValidPacket () const;
+    //// check received packet is valid
+    bool
+    isValidPacket () const;
 
-      //// receive packet (named CoLaA; SICK sensors Communication Language)
-      void
-      receiveTimPacket ();
+    //// receive packet (named CoLaA; SICK sensors Communication Language)
+    void
+    receiveTimPacket ();
 
-      //// parse received packet
-      //// convert
-      void
-      processTimPacket ();
+    void
+    parsePacketHeader (std::istringstream& ss);
+    void
+    parsePacketBody (std::istringstream& ss);
 
-      //// convert std::vector to pcl::PointCloud
-      void
-      toPointClouds ();
+    //// parse received packet
+    void
+    processTimPacket ();
 
-      void
-      processGrabbing ();
-  };
+    //// convert std::vector (distance) to pcl::PointCloud
+    void
+    toPointClouds ();
+
+    void
+    processGrabbing ();
+};
 }
--- src/tim_grabber.cpp
+++ src/tim_grabber.cpp
@@ -1,50 +1,20 @@
 /*
- * Software License Agreement (BSD License)
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
- *  Copyright (c) 2014-, Open Perception, Inc.
+ *  Copyright (c) 2020-, Open Perception
  *  Copyright (c) 2020, ysuzuki19
  *
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the copyright holder(s) nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
+ *  All rights reserved
  */
 
-
-#include "../include/pcl/io/tim_grabber.h"
-//TODO: change "xxx.h" to <xxx.h> when implement PCL and delete this line
+#include <pcl/io/tim_grabber.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 pcl::TimGrabber::TimGrabber (const boost::asio::ip::address& ipAddress,
                              const std::uint16_t port) :
     tcp_endpoint_ (ipAddress, port),
-    tim_socket_ (tim_io_context_)
+    tim_socket_ (tim_io_service_)
 {
   initialize ();
 }
@@ -61,7 +31,7 @@ pcl::TimGrabber::~TimGrabber () noexcept
 void
 pcl::TimGrabber::initialize ()
 {
-  buildLookupTable ();
+  buildLookupTables ();
 
   point_cloud_xyz_signal_ = createSignal<sig_cb_sick_tim_scan_point_cloud_xyz> ();
 
@@ -72,32 +42,34 @@ pcl::TimGrabber::initialize ()
 float
 pcl::TimGrabber::getFramesPerSecond () const
 {
-  std::lock_guard<std::mutex> lock (frequency_mutex_);
+  boost::mutex::scoped_lock lock( frequency_mutex_ );
   return (frequency_.getFrequency ());
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void
-pcl::TimGrabber::buildLookupTable () {
-  cos_dynamic_lookup_table_.resize (amount_of_data_);
-  sin_dynamic_lookup_table_.resize (amount_of_data_);
-
+pcl::TimGrabber::buildLookupTables () {
   const float angle_step = angle_range_ / static_cast<float>(amount_of_data_);
   float angle = angle_start_;
 
-  for (std::size_t i = 0; i < amount_of_data_; i++)
+  cos_dynamic_lookup_table_.reserve (amount_of_data_);
+  cos_dynamic_lookup_table_.clear ();
+  sin_dynamic_lookup_table_.reserve (amount_of_data_);
+  sin_dynamic_lookup_table_.clear ();
+
+  for (std::size_t i = 0; i < amount_of_data_; i++, angle += angle_step)
   {
-    cos_dynamic_lookup_table_[i] = std::cos (angle);
-    sin_dynamic_lookup_table_[i] = std::sin (angle);
-    angle += angle_step;
+    cos_dynamic_lookup_table_.push_back(std::cos (angle));
+    sin_dynamic_lookup_table_.push_back(std::sin (angle));
   }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void
-pcl::TimGrabber::updateLookupTable () {
-  if (cos_dynamic_lookup_table_.size () != amount_of_data_ || sin_dynamic_lookup_table_.size () != amount_of_data_)
-    buildLookupTable ();
+pcl::TimGrabber::updateLookupTables () {
+  //// note: cos, sin are the same size
+  if (cos_dynamic_lookup_table_.size () != amount_of_data_)
+    buildLookupTables ();
 }
 
 
@@ -114,20 +86,78 @@ pcl::TimGrabber::receiveTimPacket ()
   if (!tim_socket_.is_open ())
     return;
 
-  tim_socket_.send (boost::asio::buffer (scan_query_));
+  tim_socket_.send (boost::asio::buffer ("\x02sRN LMDscandata 1\x03\0"));
 
   std::this_thread::sleep_for (std::chrono::milliseconds (wait_time_milliseconds_));
 
   length_ = tim_socket_.receive (boost::asio::buffer (received_packet_));
 
   if (!isValidPacket ()) {
+    //// Countup wait_time
+    //// Because fit wait_time to network latency
     wait_time_milliseconds_++;
 
+    //// Clear invalid packet in socket
     while (!isValidPacket ())
       length_ = tim_socket_.receive (boost::asio::buffer (received_packet_));
 
-    /* If received packet is invalid, recurse. */
+    //// If received packet is invalid, recurse
     receiveTimPacket ();
+  }
+}
+
+void
+pcl::TimGrabber::parsePacketHeader (std::istringstream& ss) {
+  //// note: Protcol named CoLaA (used by SICK) has some information.
+  ////       In this Grabber, only the amount_of_data is used, so other information is truncated.
+  ////       Details of the protocol can be found at the following URL.
+
+  //// pp.87~89 (table)
+
+  //// https://cdn.sickcn.com/media/docs/7/27/927/technical_information_telegram_listing_ranging_sensors_lms1xx_lms5xx_tim2xx_tim5xx_tim7xx_lms1000_mrs1000_mrs6000_nav310_ld_oem15xx_ld_lrs36xx_lms4000_en_im0045927.pdf
+
+
+  //// By this PDF, the header contains the following information in order
+
+  /////////////////////////////////////////////////////
+  //// command type, command
+  //// version number
+  //// device number
+  //// serial number (2 value)
+  //// device status
+  //// Telegram counter
+  //// Scan counter
+  //// Time since start up
+  //// Time of transmission
+  //// Status of digital inputs (2 value)
+  //// Status of digital outputs (2 value)
+  //// Reserved
+  //// scan frequency
+  //// measurement frequency
+  //// Amount of encoder
+  //// Amount of 16 bit channels
+  //// Content
+  //// Scale factor according to IEEE754
+  //// Scale factor offset according to IEEE754
+  //// Start angle
+  //// Size of single angular step
+  //// Amount of data
+  /////////////////////////////////////////////////////
+
+  std::string str;
+  for (int i=0; i<26; ++i)
+    ss >> str;
+  // last value is Amount of data
+
+  amount_of_data_ = std::stoi (str, nullptr, 16);
+}
+
+void
+pcl::TimGrabber::parsePacketBody (std::istringstream& ss) {
+  std::string str;
+  for (auto& distance : distances_) {
+    ss >> str;
+    distance = std::stoi (str, nullptr, 16) / 1000.0;
   }
 }
 
@@ -135,25 +165,14 @@ pcl::TimGrabber::receiveTimPacket ()
 void
 pcl::TimGrabber::processTimPacket ()
 {
-  std::stringstream ss (received_packet_.data ());
-  std::string str;
+  std::istringstream ss (received_packet_.data ());
 
-  for (int i=0; i<26; ++i)
-    ss >> str;
-
-  amount_of_data_ = std::stoi (str, nullptr, 16);
+  parsePacketHeader (ss);
 
   point_cloud_xyz_ptr_->resize (amount_of_data_);
-
   distances_.resize (amount_of_data_);
-  for (auto& distance : distances_) {
-    ss >> str;
-    distance = std::stoi (str, nullptr, 16) / 1000.0;
-  }
 
-  updateLookupTable ();
-
-  toPointClouds ();
+  parsePacketBody (ss);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -161,7 +180,7 @@ void
 pcl::TimGrabber::toPointClouds () {
   point_cloud_xyz_ptr_->resize (distances_.size ());
 
-  for (int i=0; i<distances_.size (); ++i) {
+  for (std::size_t i=0; i<distances_.size (); ++i) {
     point_cloud_xyz_ptr_->points[i].x = distances_[i] * cos_dynamic_lookup_table_[i];
     point_cloud_xyz_ptr_->points[i].y = distances_[i] * sin_dynamic_lookup_table_[i];
   }
@@ -169,7 +188,7 @@ pcl::TimGrabber::toPointClouds () {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void
-pcl::TimGrabber::publishSignals ()
+pcl::TimGrabber::publishSignal ()
 {
   point_cloud_xyz_signal_->operator () (point_cloud_xyz_ptr_);
 }
@@ -184,8 +203,8 @@ pcl::TimGrabber::start ()
   is_running_ = true;
 
   try {
-    boost::asio::ip::tcp::resolver resolver (tim_io_context_);
-    tcp_endpoint_ = *resolver.resolve (tcp_endpoint_).begin ();
+    boost::asio::ip::tcp::resolver resolver (tim_io_service_);
+    tcp_endpoint_ = *resolver.resolve (tcp_endpoint_);
     tim_socket_.connect (tcp_endpoint_);
   }
   catch (std::exception& e)
@@ -231,6 +250,10 @@ pcl::TimGrabber::processGrabbing ()
 
     receiveTimPacket ();
     processTimPacket ();
-    publishSignals ();  
+
+    updateLookupTables ();
+    toPointClouds ();
+
+    publishSignal ();
   }
 }
